9a62e0648312fb2a9cec625f4219f7856783436e
Joshua Streger <Joshua.Streger@tradingscreen.com>
2020-05-13 16:12:56 -0400

    removed package

--- grid-packages/ag-grid-docs/src/php-utils/snippets.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function createSnippet($code, $language = 'js') {
-        $formattedCode = str_replace('<', '&lt;', $code);
-        $formattedCode = str_replace('>', '&gt;', $formattedCode);
-
-        return "<snippet language=\"$language\"><div ng-non-bindable>$formattedCode</div></snippet>";
-    }
